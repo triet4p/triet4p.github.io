9b84b418da5cb155b878d4379a8d2132a2f19569
Le Minh Triet <trietlm0306@gmail.com>
2025-11-18 23:44:44 +0700

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Với user page (triet4p.github.io) thì để là '/'
-})
